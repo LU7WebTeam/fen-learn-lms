ca7b36e99f0078520b349ac65f60fabc9f7ca698
Salehuddin <salehuddin@users.noreply.github.com>
2026-03-31 19:09:49 +0800

    Build: i18n About page

--- public/build/assets/menu-FCDXdzR1.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./app-PIETt5UC.js";const t=[["path",{d:"M4 5h16",key:"1tepv9"}],["path",{d:"M4 12h16",key:"1lakjw"}],["path",{d:"M4 19h16",key:"1djgab"}]],o=e("menu",t);export{o as M};
