38adeb901112471601b78f2a293a22113f7f8a5b
Salehuddin <salehuddin@users.noreply.github.com>
2026-04-03 15:35:55 +0800

    Update BM language strings

---
+++ public/build/assets/InputLabel-BdzWoSej.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B_pb-por.js";function m({value:t,className:e="",children:o,...r}){return n.jsx("label",{...r,className:"block text-sm font-medium text-foreground/90 "+e,children:t||o})}export{m as I};
