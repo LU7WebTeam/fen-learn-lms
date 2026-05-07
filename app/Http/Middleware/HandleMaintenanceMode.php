f663f42978d11737e739aaa111ca1c690339c6ce
Salehuddin <salehuddin@users.noreply.github.com>
2026-05-07 09:44:28 +0800

    Allow admin 2FA route during maintenance mode

--- app/Http/Middleware/HandleMaintenanceMode.php
+++ app/Http/Middleware/HandleMaintenanceMode.php
@@ -14,6 +14,7 @@ class HandleMaintenanceMode
         '/login',
         '/logout',
         '/register',
+        '/verify-code',
         '/forgot-password',
         '/reset-password',
         '/email',
